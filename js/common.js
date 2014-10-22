7a1d1392976c1166e0ee8eac158d4206092ec642
hraboviyvadim <hraboviyvadim@gmail.com>
2014-10-22 13:30:39 +0300

    main page responsive

--- js/common.js
+++ js/common.js
@@ -7,4 +7,15 @@ head.ready(function() {
 		autoplaySpeed: 5000
 	});
 
+	// mobile menu
+
+	$('.js-menu-btn').on('click', function(){
+		$('.js-menu').show();
+		$('body').addClass('no-scroll');
+	});
+	$('.js-menu-close').on('click', function(){
+		$('.js-menu').hide();
+		$('body').removeClass('no-scroll');
+	});
+
 });
